3f0597b3b1e53627cc114afddecdbb21b6817dc3
IainMac32 <iainhmacdonald@gmail.com>
2024-02-16 23:44:52 -0500

    backend semi working

--- backend/FinalBackend.py
+++ backend/FinalBackend.py
@@ -7,9 +7,11 @@ import numpy as np
 import os
 import getpass
 import textract
+import openai
 
 
-filepath = "exampleDoc.docx"
+
+filepath = "uploads/MarioFacts.pdf"
 api_key= "sk-"
 pc = Pinecone(api_key='d489c9e2-5765-423f-ab5a-5da2eabb2d14')  # create a Pinecone instance
 pinecone_index = pc.Index("iaintest2")
@@ -20,8 +22,6 @@ pinecone_index = pc.Index("iaintest2")
 def UploadFile(filepath,api_key):
 
     ogText = textract.process(filepath)
-    print("Doc1.0",ogText)
-    print("")
 
     from collections import namedtuple
 
@@ -47,32 +47,11 @@ def UploadFile(filepath,api_key):
     embedded_docs = embeddings.embed_documents(doc_contents)
     for i, doc in enumerate(docs):
 
-        print(len(embedded_docs[i]))
         pinecone_index.upsert(vectors=[{"id": str(i), "values": embedded_docs[i], "metadata": {"text_chunk": docs[i].page_content}}])
 
     return docs[0].page_content
 
 
-    # loader = UnstructuredWordDocumentLoader(filepath)
-    # print("1")
-    # documents = loader.load()
-    # print("2")
-    # text_splitter = CharacterTextSplitter(chunk_size=350, chunk_overlap=20)
-    # print("3")
-    # docs = text_splitter.split_documents(documents)
-    # print("4")
-    # embeddings = OpenAIEmbeddings(openai_api_key=api_key)
-    # print("5")
-    # doc_contents = [doc.page_content for doc in docs]
-    # print("6")
-
-    
-    # # Embedding and indexing
-    # embedded_docs = embeddings.embed_documents(doc_contents)
-    # for i, doc in enumerate(docs):
-
-    #     print(len(embedded_docs[i]))
-    #     pinecone_index.upsert(vectors=[{"id": str(i), "values": embedded_docs[i], "metadata": {"text_chunk": docs[i].page_content}}])
 
 
 UploadFile(filepath,api_key)
@@ -87,7 +66,6 @@ UploadFile(filepath,api_key)
 
 
 query = "Which character is bowser?"
-api_key= "sk-"
 pc = Pinecone(api_key='d489c9e2-5765-423f-ab5a-5da2eabb2d14')  
 pinecone_index = pc.Index("iaintest2")
 
@@ -99,15 +77,30 @@ def GetResponse(query, api_key):
         include_values=True
     )  # returns top_k matches
     metadata_list = []
-    
+
     for i, match in enumerate(return_vectors['matches']):
         vector_id_to_fetch = match['id']
         metadata_result = pinecone_index.fetch([vector_id_to_fetch])
         metadata = metadata_result['vectors'][vector_id_to_fetch]['metadata']
         metadata_list.append(metadata)
-    
-    return metadata_list
 
-print(GetResponse(query, api_key))
+    gpt_send = metadata_list[0]['text_chunk']
+
+
+    openai.api_key = api_key
+
+
+    completion = openai.chat.completions.create(model="gpt-4-0125-preview",
+        messages=[
+        {"role": "user", "content": "Based on this background information ONLY: " + gpt_send + "ONLY use that background information word for word to answer this question: " + query}
+    ])
+
+
+    send_back = completion.choices[0].message.content
+    print("")
+    print("")
+    return send_back
+
+print(GetResponse(query, "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"))
 
 
