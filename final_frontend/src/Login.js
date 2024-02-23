46cca177d1c1021e21ff1143e95bb77bf48f1230
japneet250 <singj93@mcmaster.ca>
2024-02-23 07:23:41 -0500

    Styled Login Page

--- final_frontend/src/Login.js
+++ final_frontend/src/Login.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './Login.css';
 import axios from 'axios';
-import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 
 const Login = ({ onLogin, message, counter }) => {
   const [username, setUsername] = useState('');
@@ -14,13 +15,50 @@ const Login = ({ onLogin, message, counter }) => {
   const [loading1, setLoading1] = useState(false);
   const [loginMsg, setLoginMsg] = useState('');
   const [messageCounter, setMessageCounter] = useState(0);
+  const [nameFieldMaxHeight, setNameFieldMaxHeight] = useState("0");
+
+  const [title, setTitle] = useState("Sign In");
+  const [signupDisabled, setSignupDisabled] = useState(true);
+  const [signinDisabled, setSigninDisabled] = useState(false);
+
+
 
 
   useEffect(() => {
     setLoading1(false);
     setLoginMsg(message);
     setMessageCounter(counter);
-  }, [message, counter]);
+    const signupbtn = document.getElementById("signupbtn");
+    const signinbtn = document.getElementById("signinbtn");
+    const nameField = document.getElementById("nameField");
+
+    nameField.style.maxHeight = nameFieldMaxHeight;
+    setTitle(title);
+    if (signupDisabled) {
+      signupbtn.classList.add("disabled");
+    } else {
+      signupbtn.classList.remove("disabled");
+    }
+    if (signinDisabled) {
+      signinbtn.classList.add("disabled");
+    } else {
+      signinbtn.classList.remove("disabled");
+    }
+  }, [message, counter , nameFieldMaxHeight, title, signupDisabled, signinDisabled]);
+
+  const handleSigninClick = () => {
+
+    if (!signinDisabled) {
+      // Show alert if sign-in state is disabled
+      alert('Click Login');}
+    else{  
+    setNameFieldMaxHeight("0");
+    setTitle("Sign In");
+    setSignupDisabled(true);
+    setSigninDisabled(false);
+  }
+};
+
 
 
   const handleLogin = () => {
@@ -31,11 +69,23 @@ const Login = ({ onLogin, message, counter }) => {
   };
 
   const registrationButton = () => {
-    setLoginMsg('');
-    setIsRegistered(!isRegistered);
-    setUsername('');
-    setPassword('');
+    if (!signupDisabled) {
+      // Show alert if sign-in state is disabled
+      alert('Click Register');
+    } else {
+      // Otherwise, proceed with sign-up functionality
+      setNameFieldMaxHeight("60px");
+      setTitle("Sign Up");
+      setSignupDisabled(false);
+      setSigninDisabled(true);
+      setLoginMsg('');
+      setIsRegistered(!isRegistered);
+      setUsername('');
+      setPassword('');
+    }
   };
+
+
 
   const handleRegistration = async () => {
     setPassCheck('');
@@ -72,6 +122,7 @@ const Login = ({ onLogin, message, counter }) => {
     }
     else {
         setPassCheck('Confirm password does not equal password')
+        
     }
     
   };
@@ -79,51 +130,69 @@ const Login = ({ onLogin, message, counter }) => {
   
 
   return (
-    <div className="login_container">
+    <body>
+      <div className="container"></div>
+      <div className="form-box">
+        <h1 id="title">{title}</h1>
+        <form>
+          <div className="input-group">
+            <div className="input-field">
+              <FontAwesomeIcon className="i" icon={faUser} />
+              <input type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
+            </div>
+            <div className="input-field">
+              <FontAwesomeIcon className="i" icon={faLock} />
+              <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+            </div>
+  
+            <div className="input-field" id="nameField" style={{ maxHeight: nameFieldMaxHeight }}>
+              <FontAwesomeIcon className="i" icon={faLock} />
+              <input type="password" placeholder="Confirm Password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} />
+            </div>
+  
+            {(signupDisabled && !loading1) && (
+              <>
+                {loginMsg && <p>{loginMsg}</p>}
+                
+                <button className='main_btn' onClick={handleLogin}>Login</button>
 
-        {isRegistered ? (
-            (
-                <div className="container">
-            <input type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
-            <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+                <h2 className='text'>New to Second Brain? </h2>
+                <h2 className='text'>Click Sign Up! </h2>
+                
+              </>
+            )}
+            {loading1 && (
+              <div className="spinner"></div>
+            )}
+  
+            {(signinDisabled && !loading) && (
+              <>
+                {passCheck && <p>{passCheck}</p>}
+                {registerMsg && <p>{registerMsg}</p>}
+                <button className='main_btn' onClick={handleRegistration}>Register</button>
 
-            {loading1 ? (
-                <div className="spinner"></div>
-            ) : (
-                <button onClick={handleLogin}>Login</button>
-                )}
-            
-
-
-            {loginMsg && <p>{loginMsg}</p>}
-            <h2>New to Second Brain?</h2>
-            <button onClick={registrationButton}>Register Here</button>
-        </div>
-            )    
-        ) : (
-            (
-                <div className="container">
-                    <input type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
-                    <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-                    <input type="password" placeholder="Confirm Password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} />
-                    
-                    {passCheck && <p>{passCheck}</p>}
-                    {registerMsg && <p>{registerMsg}</p>}
-
-                    {loading ? (
-                        <div className="spinner"></div>
-                    ) : (
-                        <button onClick={handleRegistration}>Register</button>
-                    )}
-
-                    
-                </div>
-            )
-        )}
-    </div>
+              </>
+            )}
+            {loading && (
+              <div className="spinner"></div>
+            )}
+          </div>
+          <div className="btn-field">
+            <button type="button" id="signupbtn" className={signupDisabled ? "disabled" : ""} onClick={registrationButton}>
+              Sign up
+            </button>
+            <button type="button" id="signinbtn" className={signinDisabled ? "disabled" : ""} onClick={handleSigninClick}>
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+    </body>
   );
-};
-
-
+  
+}
 
 export default Login;
+
+
+
